0e6bb64be28841b37368447cdd704cee0af67855
sius012 <dezoditing012@gmail.com>
2021-10-13 19:40:32 +0700

    menambahakan 3 tabel, semangat gess

---
+++ database/migrations/2021_10_13_123337_create_tabel_remidial.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TabelRemidial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tabel_remidial', function (Blueprint $table) {
+            $table->string('id_mapel');
+            $table->string('nama_tugas');
+            $table->date('tanggal_remded');
+            $table->string('keterangan');
+            $table->string('id_semester');
+            $table->string('id_tahun_ajaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
